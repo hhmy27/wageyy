2466d01f142bdba421b824488105c2dbe0cddeea
hhmy27 <13163369616@163.com>
2025-04-05 17:10:13 +0800

    fix: 🐛 deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import {useState} from 'react'
-import TimeInput from '@/components/TimeInput'
-import WageInput from '@/components/WageInput'
-import CountdownDisplay from '@/components/CountdownDisplay'
-import EarningsDisplay from '@/components/EarningsDisplay'
-import {Card, CardContent, CardHeader, CardTitle} from '@/components/ui/card'
-import {Separator} from '@/components/ui/separator'
+import TimeInput from '../components/TimeInput'
+import WageInput from '../components/WageInput'
+import CountdownDisplay from '../components/CountdownDisplay'
+import EarningsDisplay from '../components/EarningsDisplay'
+import {Card, CardContent, CardHeader, CardTitle} from '../components/ui/card'
+import {Separator} from '../components/ui/separator'
 
 export default function Home() {
     const [startTime, setStartTime] = useState('09:00')
